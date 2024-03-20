671898ffec904ed3824aa2b77475b1cc9f75faa9
v4roah <dafvargasro@gmail.com>
2024-03-20 16:23:59 -0500

    Form y metodo GET y POST

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('hello/<str:username>', views.hello),
     path('projects/', views.projects),
     path('tasks/', views.tasks),
+    path('create_task/', views.create_task),
 
 
 ]
